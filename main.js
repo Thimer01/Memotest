9d601f5a378679cde32d567fa32dab10782fc306
Thimer01 <thiago.merce.2016@gmail.com>
2025-06-25 16:55:02 -0300

    js

---
+++ main.js
@@ -0,0 +1,57 @@
+const elementos = ["⚽", "🎰", "🎮", "🍔", "🚒", "🏍️"];
+
+let cartas = [];
+
+let primeraCarta = null;
+
+let segundaCarta = null;
+
+let bloqueado = false;
+
+let aciertos = 0;
+
+const tablero = document.getElementById("tablero");
+
+const mensaje = document.getElementById("msj");
+
+const botonReiniciar = document.getElementById("reinicio");
+
+const mezclar = array => array.sort(() => Math.random() - 0.5);
+
+const crearTablero = () => {
+
+    tablero.innerHTML = "";
+
+    mensaje.textContent = "";
+
+    aciertos = 0;
+
+    primeraCarta = null;
+
+    segundaCarta = null;
+
+    bloqueado = false;
+
+    
+    cartas = [...elementos, ...elementos];
+
+    cartas = mezclar(cartas);
+
+
+    cartas.forEach((icono) => {
+
+        const carta = document.createElement("div");
+
+        carta.classList.add("carta");
+
+        carta.dataset.icono = icono;
+
+        carta.textContent = "";
+
+
+        carta.addEventListener("click", () => {
+
+            if (bloqueado) return;
+        })
+    })
+}
